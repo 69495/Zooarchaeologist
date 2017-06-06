b08d1bedeb26f4ce7e92272f45d673242172b4a2
karma <kimura.yuma+github@gmail.com>
2017-06-06 16:06:17 +0900

    exec.py作った boost.pyはおそらく後で消す

--- mstdn/boost.py
+++ mstdn/boost.py
@@ -1,9 +1,14 @@
 from mastodon import Mastodon
 from login import login
 
+'''
+Boostするためだけのコード
+execの方に寄せる予定
+'''
+
 def boost(target_id):
     mastodon = login()
     mastodon.status_reblog(target_id)
 
 if __name__ == '__main__':
-    main()
+    boost()
---
+++ mstdn/exec.py
@@ -0,0 +1,29 @@
+from mastodon import Mastodon
+from login import login
+
+def exec(action,target_id):
+
+    # target_id = int(input())
+    mastodon = login()
+
+    #action
+    if action == "favourite":
+        print("You are favourite "+str(target_id))
+        mastodon.status_favourite(target_id)
+    elif action == "boost":
+        print("You are boost "+str(target_id))
+        mastodon.status_reblog(target_id)
+    elif action == "status":
+        print(mastodon.status_context(target_id))
+    elif action == "delete"
+        mastodon.status_delete(target_id)
+    else:
+        print("action is enpty")
+
+"""
+    mastodon.status_reblog(target_id)
+    mastodon.status_delete(target_id)
+"""
+
+if __name__ == '__main__':
+    exec()
