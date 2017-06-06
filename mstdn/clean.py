f9d1ffd3adc6a6adf8e00e19d618be67f1319fef
karma <kimura.yuma+github@gmail.com>
2017-06-07 01:42:10 +0900

    clean.pyを完成させた

--- mstdn/clean.py
+++ mstdn/clean.py
@@ -5,23 +5,13 @@ from mining import mining
 '''
 all_destroyするためのコード
 '''
-def clean(user_id,type):
-    print("destroy start!")
+
+def clean(user_id,type=""):
     Mastodon = login()
-
-    # myaccount = Mastodon.account_verify_credentials()
-    # myaccount = Mastodon.account(user_id)
-    # print(myaccount['statuses_count'])
-
 
     user_toot = mining(user_id,type)
     for num in range(len(user_toot)):
-        print(user_toot[num]['id'])
-
-    # print(type(user_toot))
-    # for k, v in user_toot.items():
-        # print(k,v)
-    # mastodon.delete(user_id)
+        Mastodon.status_delete(user_toot[num]['id'])
 
 if __name__ == '__clean__':
     clean()
