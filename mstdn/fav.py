3b66c392307beb535aabaf842fdc167cd2912bc8
karma <kimura.yuma+github@gmail.com>
2017-06-24 00:56:42 +0900

    fav.pyを全面的に書き換えた

--- mstdn/fav.py
+++ mstdn/fav.py
@@ -2,58 +2,67 @@ from mastodon import Mastodon
 import json
 from login import login
 from out_json import jsoner
+from pprint import pprint
 import time
 
 '''
 return_typeはjson or list
 defaultはlistで返す
 '''
-'''
-現状はMastodon.pyを少し弄る必要があります
 
-'''
+def fav2(return_type="list", switch=None):
 
-def fav(return_type="list", switch=None):
+    wait_sec = 30
+    start = time.time()
+
+    e = lambda a, b : round(a-b)
+
     print(return_type + " is selected!")
 
     Mastodon = login(switch)
     my_cred = Mastodon.account_verify_credentials()
     id = my_cred['id']
+    limit = 40 #40
     print("Your id is {0}".format(id))
-    #lastestなmax_idを取得
-    initial_max_id = Mastodon.fav(None,None,limit=1)
-    initial_max_id = initial_max_id[-1]['max_id']
-    fav = Mastodon.fav(initial_max_id, None, 40)
-    print("Initial max id is {0}".format(initial_max_id))
 
-    try_num = 0
+    page = 0
+    favourites = Mastodon.favourites(None,None,limit)
+    max_id = favourites[-1]['_pagination_next']['max_id']
+    print("count: {0} ({1})".format(str(len(favourites)), str(len(favourites))))
 
     while True:
 
-        #続きのtootを取得
-        next_max_id = fav[-1]['max_id']
-        last_fav = Mastodon.fav(next_max_id, None, 40)
+        latest_favourites = Mastodon.favourites(max_id,None,limit)
 
-        fav.extend(last_fav)
-        try_num += 1
-        last_length = len(last_fav)
+        if isinstance(latest_favourites,dict):
+            pprint("Error code 429:{0} wait {1} sec...".format(latest_favourites['error'],wait_sec))
+            time.sleep(wait_sec)
+            latest_favourites = previous_favourites
+            continue
 
-        length = (len(fav) - try_num * 1)
-        print("count: {0} ({1})".format(str(length-1),last_length-1))
-        time.sleep(3)
-
-        if last_length != 41:
+        elif len(latest_favourites) < limit:
+            favourites.extend(latest_favourites)
+            page += 1
+            elapsed_time = time.time() - start
+            print("End fetch your favourites")
+            print("count: {0} time:{1}sec".format(str(len(favourites)), elapsed_time))
             break
+        else:
+            max_id = favourites[-1]['_pagination_next']['max_id']
+
+            favourites.extend(latest_favourites)
+            page += 1
+            previous_favourites = latest_favourites
+
+            print("count: {0} ({1}) time:{2}sec".format(str(len(favourites)), str(len(latest_favourites)), e(time.time(),start)))
+            time.sleep(3)
 
     if return_type == "json":
         filename = str(id) + "_fav"
-        jsoner(fav,filename)
+        jsoner(favourtes,filename)
 
     else:
-        return fav
+        return favourites
 
-    # id = int(input())
-    # mining(id)
-
-if __name__ == '__fav__':
-    fav()
+if __name__ == '__main__':
+    main()
