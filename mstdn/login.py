a099016b0b9fcbbbe4dc5dbde2d16ef06e68f120
karma <kimura.yuma+github@gmail.com>
2017-07-03 19:35:47 +0900

    loginを更新

--- mstdn/login.py
+++ mstdn/login.py
@@ -7,6 +7,12 @@ def login(account_switch=None):
         access_token = 'my_usercred_mstdn_.txt',
         api_base_url = 'https://mstdn.jp/'
         )
+    elif account_switch == 2:
+        mastodon = Mastodon(
+        client_id= 'my_clientcred_mstdn_felesitas.txt',
+        access_token = 'my_usercred_mstdn_felesitas.txt',
+        api_base_url = 'https://felesitas.cloud'
+        )
     else:
         mastodon = Mastodon(
         client_id= 'my_clientcred_mstdn.txt',
