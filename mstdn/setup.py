384ec5033a37e6feff208a61be43c692cba63d0a
karma <kimura.yuma+github@gmail.com>
2017-07-03 20:31:27 +0900

    no message

--- mstdn/setup.py
+++ mstdn/setup.py
@@ -1,8 +1,8 @@
 from mastodon import Mastodon
 def main():
-    Mastodon.create_app("mastdnPy", api_base_url = "https://felesitas.cloud", to_file = "my_clientcred_mstdn__.txt")
-    mastodon = Mastodon(client_id="my_clientcred_mstdn__.txt",api_base_url = "https://felesitas.cloud")
-    mastodon.log_in("kimura.yuma@gmail.com", "1w0pM0CDriE2KfXE5ZIt",to_file = "my_usercred_mstdn__.txt")
+    Mastodon.create_app("mastdnPy", api_base_url = "https://mstdn.jp", to_file = "my_clientcred_mstdn.txt")
+    mastodon = Mastodon(client_id="my_clientcred_mstdn.txt",api_base_url = "https://mstdn.jp")
+    mastodon.log_in("yourmail address", "password",to_file = "my_usercred_mstdn.txt")
 
 if __name__ == '__main__':
     main()
