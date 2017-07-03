a979263fbe298e8ff57dc615743b438203f1d17a
karma <kimura.yuma+github@gmail.com>
2017-07-03 19:36:35 +0900

    setup.pyを更新

--- mstdn/setup.py
+++ mstdn/setup.py
@@ -1,4 +1,8 @@
 from mastodon import Mastodon
-Mastodon.create_app("mastdnPy", api_base_url = "https://mstdn.jp", to_file = "my_clientcred_mstdn.txt")
-mastodon = Mastodon(client_id="my_clientcred_mstdn.txt",api_base_url = "https://mstdn.jp")
-mastodon.log_in("mail_address", "password",to_file = "my_usercred_mstdn.txt")
+def main():
+    Mastodon.create_app("mastdnPy", api_base_url = "https://felesitas.cloud", to_file = "my_clientcred_mstdn__.txt")
+    mastodon = Mastodon(client_id="my_clientcred_mstdn__.txt",api_base_url = "https://felesitas.cloud")
+    mastodon.log_in("kimura.yuma@gmail.com", "1w0pM0CDriE2KfXE5ZIt",to_file = "my_usercred_mstdn__.txt")
+
+if __name__ == '__main__':
+    main()
